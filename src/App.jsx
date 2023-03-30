b1d092933e3877273faa4e4ae31fa0159d355102
Brandon Hayes <eng@brandonrhayes.com>
2023-03-29 18:39:42 -0600

    Trying one more router fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {  
   Route, 
   Routes, 
-  BrowserRouter as Router
+  HashRouter as Router
 } from 'react-router-dom';
 import Home from './Home';
 import PageNotFound from './components/pagenotfound/PageNotFound';
