7f369104663154a6272098060cab6fcf86d69ddf
Brandon Hayes <eng@brandonrhayes.com>
2023-05-01 12:21:37 -0600

    UI Fixes, more tooltips

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,25 +6,27 @@ import LinkedInBadge from './LinkedInBadge'
 const Experience = () => {
   return (
     //https://www.youtube.com/watch?app=desktop&v=G-Cr00UYokU
-    <section id='experience' className='experience__container'>
-      <h5>I happen to love learning...</h5>
-      <h2>Experience</h2>
-      <p>I'm all about efficiency...</p>
+    <div className='experience__container'>
+      <section id='experience'>
+        <h5>I happen to love learning...</h5>
+        <h2>Experience</h2>
+        <p>I'm all about efficiency...</p>
 
-      {/* <div className='linkedin'>
-        <LinkedInBadge />
-      </div> */}
+        {/* <div className='linkedin'>
+          <LinkedInBadge />
+        </div> */}
 
-      <p>Please download my CV or checkout my 
-      <a href="https://linkedin.com/in/brandonrhayes" target="_blank" rel="noreferrer"> LinkedIn </a> 
-        for my latest experience.
-      </p>
+        <p>Please download my CV or checkout my 
+        <a href="https://linkedin.com/in/brandonrhayes" target="_blank" rel="noreferrer"> LinkedIn </a> 
+          for my latest experience.
+        </p>
 
-      <div className="cta">
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="https://linkedin.com/in/brandonrhayes" target="_blank" rel="noreferrer" className='btn'> View LinkedIn Profile</a>
-      </div>
-    </section>
+        <div className="cta">
+          <a href={CV} download className='btn'>Download CV</a>
+          <a href="https://linkedin.com/in/brandonrhayes" target="_blank" rel="noreferrer" className='btn'> View LinkedIn Profile</a>
+        </div>
+      </section>
+    </div>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import Tooltip from '@mui/material/Tooltip';
 import {FaInstagram} from 'react-icons/fa'
 import {FaLinkedinIn} from 'react-icons/fa'
 import {FaTwitter} from 'react-icons/fa'
 import {FaGoodreadsG} from 'react-icons/fa'
 import {FaGithub} from 'react-icons/fa'
 
-
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
+      <Tooltip className='tooltip' title = 'Connect with Brandon on LinkedIn'>
         <a href="https://linkedin.com/in/brandonrhayes" target="_blank" rel="noreferrer"><FaLinkedinIn size={35}/></a>
+      </Tooltip>
+
+      <Tooltip className='tooltip' title = 'Connect with Brandon on Instagram'>
         <a href="https://www.instagram.com/brandonrhayes" target="_blank" rel="noreferrer"><FaInstagram size={35}/></a>
+      </Tooltip>
+
+      <Tooltip className='tooltip' title = 'Connect with Brandon on Twitter'>
         <a href="https://twitter.com/brandonrhayes" target="_blank" rel="noreferrer"><FaTwitter size={35}/></a>
+      </Tooltip>
+
+      <Tooltip className='tooltip' title = 'Checkout what Brandon is Reading'>
         <a href="https://www.goodreads.com/brandonrhayes" target="_blank" rel="noreferrer"><FaGoodreadsG size={35}/></a>
+      </Tooltip>
+
+      <Tooltip className='tooltip' title = 'Checkout what Brandon has been Developing'>
         <a href="https://www.github.com/brandonrhayes" target="_blank" rel="noreferrer"><FaGithub size={35}/></a>
+      </Tooltip>
     </div>
   )
 }
