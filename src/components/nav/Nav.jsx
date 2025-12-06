50d1d9b874d75de62098244744a6718341a1e045
Brandon Hayes <eng@brandonrhayes.com>
2025-12-06 11:51:14 -0700

    update more packages

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { useState } from "react";
 import "./nav.css";
 import { FaHome } from "react-icons/fa";
 import { FaUserAstronaut, FaHandHoldingHeart } from "react-icons/fa";
